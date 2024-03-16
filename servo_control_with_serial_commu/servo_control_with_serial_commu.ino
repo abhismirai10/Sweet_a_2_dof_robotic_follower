015be9eda6f72399da984f7f22cdefdc4da649af
Abhi Chothani <abhismirai10@gmail.com>
2024-03-16 05:22:43 -0400

    complete the project now Proposnal controller

--- servo_control_with_serial_commu/servo_control_with_serial_commu.ino
+++ servo_control_with_serial_commu/servo_control_with_serial_commu.ino
@@ -1,13 +1,17 @@
-// servo control using a serial communication
+
 #include <Servo.h>
 
 Servo yaw_servo;
 Servo pitch_servo;
 
-//xy_servo is yaw and xz_servo is pitch 
-//same for potentiometer
+int yaw_angle = 90;
+int pitch_angle = 90;
+
 #define xy_servo 9
 #define xz_servo 10
+
+unsigned long previousMillis = 0; // will store last time servo was updated
+const long interval = 30;        // interval at which to update servo (milliseconds)
 
 void setup() {
   Serial.begin(9600);
@@ -15,35 +19,33 @@ void setup() {
   pitch_servo.attach(xz_servo);
 }
 
-void loop() {
-  if (Serial.available() > 0) {
-    String command = Serial.readStringUntil('\n'); // Read the full command
-
-    int yPos = command.indexOf('Y');
-    int pPos = command.indexOf('P');
-
-    if (yPos != -1 && pPos != -1) {
-      // Extract yaw position, knowing 'P' follows 'Y'
-      int yawPosition = command.substring(yPos + 1, pPos).toInt();
-
-      // Extract pitch position, from 'P' to the end
-      int pitchPosition = command.substring(pPos + 1).toInt();
-
-      // Set servo positions if within valid range
-      if (yawPosition >= 10 && yawPosition <= 170) 
-      {
-        yaw_servo.write(yawPosition);
-      }
-      if (pitchPosition >= 10 && pitchPosition <= 170) 
-      {
-        pitch_servo.write(pitchPosition);
-      }
-
-      delay(500);
-    }
+void updateServo(Servo& servo, int& angle, int error) {
+  if (abs(error) > 15) { // Only adjust if error is significant
+    angle -= error / 38; // Example proportional control factor
+    angle = constrain(angle, 30, 150); // Constrain angle to prevent over-rotation
+    servo.write(angle);
   }
 }
 
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n'); // Read the command until newline
+    int commaIndex = command.indexOf(',');
+    
+    if (commaIndex != -1) {
+      int yaw_error = command.substring(0, commaIndex).toInt();
+      int pitch_error = command.substring(commaIndex + 1).toInt();
+      
+      if(currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;   
+        updateServo(yaw_servo, yaw_angle, yaw_error);
+        updateServo(pitch_servo, pitch_angle, pitch_error);
+      }
+    }
+  }
+}
 
 
 
